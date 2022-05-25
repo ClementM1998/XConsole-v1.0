10b19d27e796574527acac08a7388f9dc234eba4
Clement Okie <83030323+ClementM1998@users.noreply.github.com>
2022-05-25 20:43:15 +0800

    Add files via upload

---
+++ test/Main.java
@@ -0,0 +1,12 @@
+package test;
+
+import xconsole.Console;
+
+public class Main {
+    public static void main(String[] args) {
+		Console.clear();
+        Console.println("This is simple Console library");
+        Console.pause();
+        Console.exit();
+    }
+}
